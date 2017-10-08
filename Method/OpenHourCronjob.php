9a5003d5ee36f5462186f2a51e319981dfe79f63
gizmore <gizmore@wechall.net>
2017-10-08 20:04:38 +0200

    6.05_fixes

--- Method/OpenHourCronjob.php
+++ Method/OpenHourCronjob.php
@@ -16,7 +16,7 @@ final class OpenHourCronjob extends MethodCronjob
     {
         foreach (get_declared_classes() as $className)
         {
-            if (is_subclass_of($className, 'GDO\\DB\\GDO'))
+            if (is_subclass_of($className, 'GDO\\Core\\GDO'))
             {
                 $class = new ReflectionClass($className);
                 if (!$class->isAbstract())
