87856b8fdbbb04c2c8af8eed600474f7ffc58166
gizmore <gizmore@wechall.net>
2018-10-27 02:13:58 +0200

    6.08-r67-i18n-lazy-label

--- tpl/form/open_hours.php
+++ tpl/form/open_hours.php
@@ -9,7 +9,7 @@ $field instanceof GDT_OpenHours;
  ng-controller="GDOOpenHoursCtrl"
  ng-init='initJSON(<?=json_encode($field->configJSON()); ?>)'>
 
-  <label for="form[<?= $field->name; ?>]"><?= $field->label; ?></label>
+  <label for="form[<?= $field->name; ?>]"><?= $field->displayLabel(); ?></label>
   <?= GDT_Icon::iconS('schedule'); ?>
 <!--   <input type="text" ng-model="data.openHours.display" nag-click="openHoursDialog($event)" /> -->
 
