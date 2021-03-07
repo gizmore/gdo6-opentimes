0eb808243542837c5efc18481ef90f93016aeb87
gizmore <gizmore@wechall.net>
2021-03-07 03:28:40 +0100

    6.10.1-r1011_themeswitching

--- Module_OpenTimes.php
+++ Module_OpenTimes.php
@@ -2,7 +2,16 @@
 namespace GDO\OpenTimes;
 
 use GDO\Core\GDO_Module;
+use GDO\Angular\Module_Angular;
+use GDO\Core\Application;
 
+/**
+ * Include open times library.
+ * If angular or material is used, angular ctrls will be loaded.
+ * @author gizmore
+ * @version 6.10.1
+ * @since 6.3.0
+ */
 final class Module_OpenTimes extends GDO_Module
 {
 	public $module_priority = 20;
@@ -10,10 +19,15 @@ final class Module_OpenTimes extends GDO_Module
 	
 	public function onIncludeScripts()
 	{
-		$this->addJavascript('bower_components/opening_hours/opening_hours.js');
 		if (module_enabled('Angular'))
 		{
-			$this->addJavascript('js/gwf-open-hours-ctrl.js');
+		    if (Module_Angular::instance()->cfgIncludeScripts() ||
+		        Application::instance()->hasTheme('material'))
+		    {
+    			$this->addJavascript('js/gwf-open-hours-ctrl.js');
+		    }
 		}
+		$this->addJavascript('bower_components/opening_hours/opening_hours.js');
 	}
+
 }
