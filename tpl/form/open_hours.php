c8ef8f0f2ed0875d08306952dc8c3e48044b9db5
gizmore <gizmore@wechall.net>
2021-05-06 01:23:41 +0200

    6.10.2-r1071

--- tpl/form/open_hours.php
+++ tpl/form/open_hours.php
@@ -2,7 +2,7 @@
 use GDO\OpenTimes\GDT_OpenHours;
 /** @var $field GDT_OpenHours **/
 ?>
-<div class="gdo-container<?= $field->classError(); ?>">
+<div class="gdt-container<?= $field->classError(); ?>">
   <?= $field->htmlIcon(); ?>
   <label <?=$field->htmlForID()?>><?= $field->displayLabel(); ?></label>
   <textarea
